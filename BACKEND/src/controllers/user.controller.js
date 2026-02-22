9e61eadaa7bd78100755c4510122b03b1235a3c5
kalyan kumar <pothugantikalyankumar71@gmail.com>
2026-02-22 21:24:15 +0530

    production same site error

--- BACKEND/src/controllers/user.controller.js
+++ BACKEND/src/controllers/user.controller.js
@@ -99,7 +99,7 @@ const loginUser = asyncHandler(async (req, res)=>{
     const options = {
         httpOnly: true,
         // secure: true,
-        // sameSite: "none"  /////production if needed 
+        sameSite: "none",  /////production if needed 
         secure: process.env.SECURE,
 
     }
@@ -125,7 +125,7 @@ const logoutUser = asyncHandler(async (req , res)=>{
     const options = {
         httpOnly:true,
         // secure:true,
-        // sameSite: "none"
+        sameSite: "none",
         secure: process.env.SECURE,
     }
 
